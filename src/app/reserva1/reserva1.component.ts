3eba1df7e5f84b86bd8375bcc6bfb1ef3fde14ca
Nix-ui <matheo.quiroga@ucb.edu.bo>
2025-06-11 16:28:09 -0400

    reservs

---
+++ src/app/reserva1/reserva1.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from "../home/navbar/navbar.component";
+import { ReservarFechaComponent } from '../reservar-fecha/reservar-fecha.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+
+@Component({
+  selector: 'app-reserva1',
+  imports: [NavbarComponent, RouterOutlet, RouterLink],
+  templateUrl: './reserva1.component.html',
+  styleUrl: './reserva1.component.scss'
+})
+export class Reserva1Component {
+
+}
